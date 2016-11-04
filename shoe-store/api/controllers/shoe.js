7feb6c8696d1d66007f578c1461c824b3df1d2a6
a1exLemesios <you@example.com>
2016-11-04 11:13:14 +0200

    Powershell script training

--- shoe-store/api/controllers/shoe.js
+++ shoe-store/api/controllers/shoe.js
@@ -1,42 +1,60 @@
 'use strict';
    // Include our "db"
-   var db = require('../../config/db')();
-   // Exports all the functions to perform on the db
-   module.exports = {post, getOne, update, delShoe, getAll}; // , , getOne, ,
+   const db = require("mysql");
 
-   function post(req, res , next) {
-     const model = req.body.model.value;
-     const size = req.body.size.value;
-     console.log(req.swagger.params.shoe.value);
-     res.json({success: 1, description: "Movie updated!"});
-   }
+   const pool  = db.createPool({
+    connectionLimit : 10,
+    host            : "127.0.0.1",
+    user            : "root",
+    password        : '',
+    port            : 3306,
+    database        : "shoe-store"
+  });
 
-   function getOne(req , res , next) {
-     const id = req.swagger.params.id.value;
-     console.log(req.swagger.params);
-     const shoe = db.find(id);
-     if(shoe) {
-       res.json(shoe);
-     }
-     else {
-       res.status(204).send();
-     }
-   }
+   module.exports = {post, getOne, update, delShoe, getByBrand};
 
-   function update(req, res, next) {
-     const id = req.swagger.params.id.value;
-     const model = req.swagger.params.model.value;
-     const size = req.swagger.params.size.value;
-     res.json(model);
-   }
+ function post(req, res , next) {
+   const shoe = (req.swagger.params.shoe.value);
+   pool.query('INSERT INTO shoe SET ?', shoe ,  function(err, rows, fields) {
+     if (err) throw err;
+     console.log(rows);
+     res.json({success: 1, description: "Shoe inserted succesfully"});
+   });
+ }
+
+ function getOne(req , res , next) {
+   const id = req.swagger.params.id.value;
+   pool.query('SELECT * FROM shoe WHERE idShoe = ?', id , function(err, rows, fields) {
+     if (err) throw err;
+     console.log(rows);
+   });
+   res.json({success: 1, description: "Shoe retrieved succesfully"});
+ }
+
+ function update(req, res, next) {
+   const id = req.swagger.params.id.value;
+   const shoe = req.swagger.params.shoe.value;
+   pool.query('UPDATE shoe SET model = ?, size = ? WHERE idshoe = ?', [shoe.model, shoe.size, id], function(err, rows, fields) {
+     if (err) throw err;
+     console.log(rows);
+     res.json({success: 1, description: "Shoe updated succesfully"});
+   });
+ }
 
 function delShoe(req, res, next) {
   const id = req.swagger.params.id.value;
-  res.json(id);
+  pool.query(`DELETE FROM shoe WHERE idshoe = ?`, id, function(err, rows, fields) {
+    if (err) throw err;
+    console.log(rows);
+  });
+     res.json({success: 1, description: "Shoe deleted succesfully"});
 }
 
-function getAll(req, res, next) {
-  const id = req.swagger.params.id.value;
-  console.log(req.swagger);
-  res.json({success: 1, description: "Shoe found!"});
+function getByBrand(req, res, next) {
+  const idBrand = req.swagger.params.idBrand.value;
+  pool.query(`SELECT * FROM shoe WHERE idBrand = ?`, idBrand, function(err,rows,fields) {
+    if (err) throw err;
+    console.log(rows)
+  });
+  res.json({success: 1, description: rows});
 }
